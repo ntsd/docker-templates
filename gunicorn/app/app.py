41a702c185c89ff45ea42f568348ef718574341f
ntsd <jo06942@gmail.com>
2019-10-01 14:54:45 +0700

    Updated init app for gunicorn

---
+++ gunicorn/app/app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+print(os.environ)
+
+if __name__ == '__main__':
+    if eval(os.getenv('DEBUG', True)):
+        port = int(os.environ.get('PORT', 80))
+        app.run(host='0.0.0.0', port=port, threaded=True, debug=True, use_reloader=True)
+    else:
+        port = int(os.environ.get('PORT', 80))
+        app.run(host='0.0.0.0', port=port, threaded=True, debug=False, use_reloader=False)
