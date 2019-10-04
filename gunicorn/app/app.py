9b69680b559d780a91fe8a2c7e552b1e0c6ec8f4
ntsd <jo06942@gmail.com>
2019-10-04 23:48:43 +0700

    Fixed gunicorn python docker doesn't have g++

--- gunicorn/app/app.py
+++ gunicorn/app/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 if __name__ == '__main__':
     if eval(os.getenv('DEBUG', 'True')):
         port = int(os.environ.get('PORT', 80))
+        print("App is running on port " + str(port))
         app.run(host='0.0.0.0', port=port, threaded=True, debug=True, use_reloader=True)
     else:
         port = int(os.environ.get('PORT', 80))
